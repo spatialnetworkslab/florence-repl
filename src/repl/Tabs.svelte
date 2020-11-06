f5014c6f2eeaf164563bbec3252ab29b49442a90
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-06 14:57:33 +0100

    feat: tabs

---
+++ src/repl/Tabs.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+
+	export let tabs = []
+	export let current = 0
+  
+  const dispatch = createEventDispatcher()
+</script>
+
+<ul>
+	{#each tabs as { name, type, id }}
+		<li class:active={id === current} on:click={() => dispatch('select', id)}>
+			{name}.{type}
+		</li>
+	{/each}
+	<li><button on:click={() => dispatch('new')}>+</button></li>
+</ul>
+
+<style>
+	.active {
+		font-weight: 900;
+		text-decoration: underline;
+	}
+</style>
