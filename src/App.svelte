50e9dc93a3bcf7683cdc1cf492138915c1628df2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-06 14:18:01 +0100

    feat: folder structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
+  import REPL from './repl/REPL.svelte'
+
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <REPL />
 </main>
 
 <style>
---
+++ src/repl/REPL.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Input from './Input.svelte'
+	import Output from "./Output.svelte";
+	import type { Component } from "./types";
+
+	let components: Component[] = [
+		{
+			id: 0,
+			name: "App",
+			type: "svelte",
+			source: `<script>
+  import Component from './Component1.svelte';
+  import testDep from 'my-test-dependency'
+
+  testDep()
+<\/script>
+
+<Component />
+	`,
+		},
+		{
+			id: 1,
+			name: "Component1",
+			type: "svelte",
+			source: "<h1>Hello</h1>",
+		},
+	];
+
+	let current: number = 0;
+
+	const worker = new Worker("./worker.js");
+
+	let compiled;
+
+	worker.addEventListener("message", (event) => {
+		compiled = event.data;
+	});
+
+	function compile(_components: Component[]): void {
+		worker.postMessage(_components);
+	}
+
+	$: compile(components);
+</script>
+
+<main>
+	<Input bind:components bind:current />
+	<Output {compiled} />
+</main>
