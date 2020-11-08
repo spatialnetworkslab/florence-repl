79f2f6e991b2f84f16c75c93be2d2d86277c713e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-08 21:21:30 +0100

    _

--- src/repl/output/Output.svelte
+++ src/repl/output/Output.svelte
@@ -1,4 +1,6 @@
 <script>
+  import srcdoc from './srcdoc.js'
+
 	export let bundled
 
 	let iframe
@@ -8,35 +10,6 @@
 	}
 
 	$: iframe && bundled && update(bundled)
-
-	const srcdoc = `
-<!doctype html>
-<html>
-	<head>
-		<script type="module">
-
-			let c;
-
-			function update(source) {
-				const blob = new Blob([source], { type: 'text/javascript' });
-				const url = URL.createObjectURL(blob);
-
-				import(url).then(({ default: App }) => {
-					if (c) c.$destroy();
-
-					document.body.innerHTML = '';
-					c = new App({ target: document.body })
-				})
-			}
-
-			window.addEventListener('message', event => {
-				update(event.data)
-			}, false)
-		<\/script>
-	</head>
-	<body></body>
-</html>
-`
 </script>
 
 <section>
