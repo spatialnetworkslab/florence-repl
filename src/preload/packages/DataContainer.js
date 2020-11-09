5adc8d6a45ec0a47ffb90bd05aff92419f5ad154
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-09 16:49:53 +0100

    feat: update package metadata

---
+++ src/preload/packages/DataContainer.js
@@ -0,0 +1,6 @@
+const SNLAB_URL = 'https://cdn.jsdelivr.net/gh/spatialnetworkslab'
+
+export default {
+  name: '@snlab/florence-datacontainer',
+  url: `${SNLAB_URL}/florence-datacontainer@HEAD/dist/florence-datacontainer.esm.js`
+}
