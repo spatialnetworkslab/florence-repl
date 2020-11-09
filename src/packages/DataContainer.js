995a41f0b8aba1e6165414cbefc0820454bb0f05
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-09 18:10:32 +0100

    fix: using specific datacontainer commit for now

--- src/packages/DataContainer.js
+++ src/packages/DataContainer.js
@@ -2,5 +2,5 @@ const SNLAB_URL = 'https://cdn.jsdelivr.net/gh/spatialnetworkslab'
 
 export default {
   name: '@snlab/florence-datacontainer',
-  url: `${SNLAB_URL}/florence-datacontainer@HEAD/dist/florence-datacontainer.esm.js`
+  url: `${SNLAB_URL}/florence-datacontainer@5d61c54ae81b4c82a4811427dae8bb12cf309401/dist/florence-datacontainer.mjs`
 }
