3301e12f40c44968fcae66f1168c471b79960146
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-23 14:24:27 +0100

    fix: florence preload behavior

--- src/bundler/plugins/prebundler/getPackageURL.js
+++ src/bundler/plugins/prebundler/getPackageURL.js
@@ -5,9 +5,13 @@ export default async function getPackageURL (packageName, cdn) {
   const pkgUrl = `${url}/package.json`
   const pkg = JSON.parse(await fetchPackage(pkgUrl))
 
-  // get an entry point from the pkg.json - first try svelte, then modules, then main
-  if (pkg.svelte || pkg.module || pkg.main) {
-    // use the above url minus `/package.json` to resolve the URL
-    return new URL(pkg.svelte || pkg.module || pkg.main, `${url}/`).href
+  if (packageName === '@snlab/florence') {
+    return new URL(pkg.module, `${url}/`).href
+  } else {
+    // get an entry point from the pkg.json - first try svelte, then modules, then main
+    if (pkg.svelte || pkg.module || pkg.main) {
+      // use the above url minus `/package.json` to resolve the URL
+      return new URL(pkg.svelte || pkg.module || pkg.main, `${url}/`).href
+    }
   }
 }
