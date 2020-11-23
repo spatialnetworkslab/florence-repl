7f72a49a564686dfa7e9e6b0cb7815ec3996915a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-23 15:25:40 +0100

    fix: loading speed of florence

--- public/workers/bundler.js
+++ public/workers/bundler.js
@@ -170,11 +170,25 @@ function prebundler ({ cache }) {
     async load (importee, importer) {
       if (importee in cache) return cache[importee].code
 
-      const prebundled = await prebundle({ packageName: importee });
+      // if (importee === '@snlab/florence') {
+      //   const prebundled = await fetchPackage('https://cdn.skypack.net/@snlab/florence')
+      // }
+
+      // const prebundled = await prebundle({ packageName: importee })
+
+      const prebundled = importee === '@snlab/florence'
+        ? await fetchFlorence()
+        : await prebundle({ packageName: importee });
+
       cache[importee] = prebundled;
       return cache[importee].code
     }
   }
+}
+
+async function fetchFlorence () {
+  const code = await fetchIfUncached('https://cdn.skypack.dev/-/@snlab/florence@v0.1.22-0GlOErMo1VIYJxFjzNJK/dist=es2020/@snlab/florence.js');
+  return { code }
 }
 
 var json = {
--- src/bundler/plugins/prebundler/prebundler.js
+++ src/bundler/plugins/prebundler/prebundler.js
@@ -1,4 +1,5 @@
 import prebundle from './prebundle.js'
+import fetchPackage from '../../fetchPackage.js'
 
 export default function prebundler ({ cache }) {
   return {
@@ -9,9 +10,23 @@ export default function prebundler ({ cache }) {
     async load (importee, importer) {
       if (importee in cache) return cache[importee].code
 
-      const prebundled = await prebundle({ packageName: importee })
+      // if (importee === '@snlab/florence') {
+      //   const prebundled = await fetchPackage('https://cdn.skypack.net/@snlab/florence')
+      // }
+
+      // const prebundled = await prebundle({ packageName: importee })
+
+      const prebundled = importee === '@snlab/florence'
+        ? await fetchFlorence()
+        : await prebundle({ packageName: importee })
+
       cache[importee] = prebundled
       return cache[importee].code
     }
   }
+}
+
+async function fetchFlorence () {
+  const code = await fetchPackage('https://cdn.skypack.dev/-/@snlab/florence@v0.1.22-0GlOErMo1VIYJxFjzNJK/dist=es2020/@snlab/florence.js')
+  return { code }
 }
